7b7d3ca9f8a4410adaa0e5f7bbea43fc59a12d0a
Parashar <devdewance@gmail.com>
2016-10-16 23:40:56 +0530

    fff

--- src/com/darauli/pdp/controller/LoginController.java
+++ src/com/darauli/pdp/controller/LoginController.java
@@ -1,12 +1,11 @@
 package com.darauli.pdp.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -60,7 +59,27 @@ public class LoginController {
 	 return "login";
 		
 	}
-}	
+	
+	@RequestMapping("/registration")
+	public String getSignUpPage(Model model){
+		model.addAttribute("registrion", new Registration());
+		return "registration";
+	}
+	
+	@RequestMapping(value="/registration", method=RequestMethod.POST)
+	public String ProcessSignUp(@ModelAttribute("registration") Registration user, Model model , BindingResult result){
+		if(result.hasErrors()){
+			return "redirect:/registration";
+		}
+		int i =	loginDao.registerUser(user);
+		if(i>0){
+			model.addAttribute("userd", i);
+			return "redirect:/";	
+		}else{
+			return "redirect:/registration";
+			}
+		
+	}}	
 		
 		
 	
